39613d9ae29db8bdaa3b0d2e38c1891f1853a9e6
AAniketA <139612700+AAniketA@users.noreply.github.com>
2023-08-05 14:11:37 +0530

    final modification

--- DHE.m
+++ DHE.m
@@ -1,27 +1,30 @@
-clear,clc;
+%Read and Display original Image
 I=imread("cameraman.tif");
 Size=size(I);
 figure();
 subplot(2,2,1);
 imshow(I);
 subplot(2,2,2);
-Histo1=Histogram_Function(I);
+
+Histo1=Histogram_Function(I);%Calculates Histogram
 histogram(I);
 A=mean(I);
 Avg1=mean(A);
+
 Dh=zeros(1,256);
 Ddh=zeros(1,256);
 
+%Calculate first order difference
 for l=2:255
     Dh(l)=Histo1(l+1)-Histo1(l-1);
 end
-% subplot(2,2,2);
-% plot(Dh);
+
+%Calculate second order difference
 for m=2:254
     Ddh(m)=Dh(m+1)-Dh(m-1);
 end
-% subplot(2,2,3);
-% plot(Ddh);
+
+%Find all local maxima
 o=0;
 LocalMax=zeros(1,256);
 for n=2:256
@@ -33,7 +36,7 @@ for n=2:256
     end
 end
 
-
+%Select dominant maxima from all local maxima
 q=0;
 for p=1:51
     if LocalMax(p+1)-LocalMax(p)==1;
@@ -48,6 +51,8 @@ for p=1:51
 end
 t=1;
 s=1;
+
+%Find locations of dominant maxima
 Maxima(1)=TrueMax(1);
 for r=2:q
     if TrueMax(r)-TrueMax(t)>=21
@@ -57,8 +62,7 @@ for r=2:q
     end
 end
 
-% subplot(2,2,4);
-% plot(Maxima);
+%Equilization of divided histogram
 Start=1;
 for a=1:s
     F(1,[Start:Maxima(a)])=cumsum(Histo1(1,[Start:Maxima(a)]));
@@ -66,38 +70,22 @@ for a=1:s
 end
 F(1,[Start:256])=cumsum(Histo1(1,[Start:256]));
 
-% F(1,[1:Avg1])=cumsum(Histo1(1,[1:Avg1]));
-% F(1,[Avg1+1:256])=cumsum((Histo1(1,[Avg1+1:256])));
-% subplot(2,2,2);
-% plot(F);
-% 
-% % K=floor((255*F)/(Size(1)*Size(2)));
 Start=1;
 for a=1:s
-%     K(1,[Start:Maxima(a)])=floor((Maxima(a)*F(1,[Start:Maxima(a)]))/F(1,Maxima(a)));
-%     Start=Maxima(a)+1;
         K(1,[Start:Maxima(a)])=floor(Start+((Maxima(a)-Start)*F(1,[Start:Maxima(a)]))/F(1,Maxima(a)));
         Start=Maxima(a)+1;
-%         K(1,[Avg1+1:256])=floor(119+(136*F(1,[Avg1+1:256]))/F(1,256));
 end
 K(1,[Maxima(s):256])=floor(Maxima(s)+((255-Maxima(s))*F(1,[Maxima(s):256]))/F(1,256));
-% K(1,[1:Avg1])=floor((255*F(1,[1:Avg1]))/F(1,Avg1));
-% K(1,[Avg1+1:256])=floor((255*F(1,[Avg1+1:256]))/F(1,256));
-% 
+
 I2=I;
 for i=1:Size(1)
     for j=1:Size(2)
         I2(i,j)=K(I(i,j));
     end
 end
-B=mean(I2);
-Avg2=mean(B);
+
+%Display modified image
 subplot(2,2,3);
 imshow(I2);
 subplot(2,2,4);
 histogram(I2);
-
-
-
-
-
